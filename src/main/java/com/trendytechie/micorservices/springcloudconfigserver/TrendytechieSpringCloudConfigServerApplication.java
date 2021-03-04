2e597ec0c2e86f1984975ddbe4e69dcf59495cfe
Siva Kumar <siva.peketi@resolver.com>
2021-03-04 15:23:23 +0530

    Initial commit

---
+++ src/main/java/com/trendytechie/micorservices/springcloudconfigserver/TrendytechieSpringCloudConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.trendytechie.micorservices.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendytechieSpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendytechieSpringCloudConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trendytechie/micorservices/springcloudconfigserver/TrendytechieSpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trendytechie.micorservices.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendytechieSpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
