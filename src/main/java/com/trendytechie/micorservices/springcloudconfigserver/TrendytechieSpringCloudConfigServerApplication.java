697827795e2fc5c3c786f59bd94db0c8140de571
Siva Kumar <siva.peketi@resolver.com>
2021-09-20 14:33:10 +0530

    actuator configuration

--- src/main/java/com/trendytechie/micorservices/springcloudconfigserver/TrendytechieSpringCloudConfigServerApplication.java
+++ src/main/java/com/trendytechie/micorservices/springcloudconfigserver/TrendytechieSpringCloudConfigServerApplication.java
@@ -2,8 +2,11 @@ package com.trendytechie.micorservices.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
 
 @SpringBootApplication
+@EnableConfigServer
 public class TrendytechieSpringCloudConfigServerApplication {
 
 	public static void main(String[] args) {
